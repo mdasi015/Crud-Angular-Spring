9fbd30c4ce9bc342f315e418f63e05e403853b24
Mariana <marianads18@hotmail.com>
2021-12-28 17:22:09 -0300

    api e proxy

--- crud-spring/src/main/java/com/mariana/crudspring/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/mariana/crudspring/CrudSpringApplication.java
@@ -1,13 +1,31 @@
 package com.mariana.crudspring;
 
+import com.mariana.crudspring.model.Course;
+import com.mariana.crudspring.repository.CourseRepository;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner initDataBase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+
+			Course c = new Course();
+			c.setName("Angular com Spring");
+			c.setCategory("front-end");
+
+			courseRepository.save(c);
+		};
 	}
 
 }
--- crud-spring/src/main/java/com/mariana/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/mariana/crudspring/controller/CourseController.java
@@ -3,18 +3,29 @@ package com.mariana.crudspring.controller;
 import java.util.List;
 
 import com.mariana.crudspring.model.Course;
+import com.mariana.crudspring.repository.CourseRepository;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/courses")
+
+@Component
 public class CourseController {
+
+    private final CourseRepository courseRepository;
+
+    public CourseController(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
 
     //@RequestMapping(method = RequestMethod.POST)
     @GetMapping
-    public List<Course> list() {
-        return null;
+    public @ResponseBody List<Course> list() {
+        return courseRepository.findAll();
     }
 }
